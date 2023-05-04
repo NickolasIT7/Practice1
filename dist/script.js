0141642517d4fba6026d617f626cedf7074e53dd
NickolasIT7 <justdoitagainnick@gmail.com>
2023-05-05 02:20:07 +0300

    1

---
+++ dist/script.js
@@ -0,0 +1,36 @@
+// let c = +(prompt('Enter number') as string)
+// alert (c**2);
+// let d = +(prompt('Enter first number') as string)
+// let f = +(prompt('Enter last number') as string)
+// alert((d + f) / 2);
+//  let c = +(prompt('Введите длину стороны квадрата') as string)
+//  alert (c**2);
+//  let d = +(prompt('Введите количество километров') as string)
+//  var k = 0.621371
+//  alert ('количество миль, ' +(k*d));
+// let f = +(prompt ('enter first number') as string)
+// let g = +(prompt ('enter sercond number') as string)
+// console.log(f+g)
+// console.log(f-g)
+// console.log(f*g)
+// console.log(f/g);
+//  let a = +(prompt ('enter first number') as string)
+//  let b = +(prompt ('enter sercond number') as string)
+//  alert (-b/a);
+// let n = +(prompt('введите часы') as string)
+// let m = +(prompt('введите минуты') as string)
+// let n1 = 23-n
+// let m1 = 60-m
+// alert ('количество часов до следующего дня, '+(n1) +' количество минут, ' +(m1))
+//  let b = +(prompt('введите 3х значное число') as string) 
+//  b = Math.floor(b/10)%10;
+//  alert (b);
+// let a = +(prompt ('введите 5ти значное число') as string)
+// let b = a % 10
+// a = Math.floor(a/10)
+// a = (b + a)
+// alert(a);
+// let b = 250
+// let a = +(prompt ('введите сумму общих продаж') as string)
+// a = a * 0.1
+// alert('зарплата за месяц $, ' +(a + b));
